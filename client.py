1bd219558a6486518e18292506123c496768ba2f
aum05 <78663514+aum05@users.noreply.github.com>
2021-02-12 17:20:30 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,54 +1,76 @@
 import threading
 import time
 import random
+import sys
 
 import socket
 
 # client for root server
 try:
     crs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("[CRS]: Client socket created")
+    print("[CRS]: Client socket for RS created")
 except socket.error as err:
     print('socket open error: {} \n'.format(err))
     exit()
 
-port = 50001
-localhost_addr = socket.gethostbyname(socket.gethostname())
-server_binding = (localhost_addr, port)
-crs.connect(server_binding)
-csockid, addr = crs.accept()
+# port and hostname from cmd line argument
+rs_port = int(sys.argv[2])
+rshost_addr = socket.gethostbyname(sys.argv[1])
 
-msg = "qtsdatacenter.aws.com"
-print(msg)
-csockid.send(msg.encode('utf-8'))
+# connect to rs
+rs_binding = (rshost_addr, rs_port)
+crs.connect(rs_binding)
 
+# read input file
+with open('PROJI-HNS.txt', 'r') as f:
+    read_file = f.readlines()
+
+# send a intro message to the client.
+n = random.randint(0,len(read_file)-1)
+msg = read_file[n].rstrip("\r\n")
+crs.send(msg.encode('utf-8'))
+
+# Receive data from the server
+data_from_rs = crs.recv(100)
+decoded_msg = data_from_rs.decode('utf-8')
+print("[C]: Data received from root-server: " + str(decoded_msg))
+
+# write received data to outupt file if DNS is resolved
+if "A" in decoded_msg:
+    with open('resolve.txt', 'a') as text_file:
+        text_file.write(decoded_msg + "\n")
+
+name = "localhost - NS".split(" ")
 
 # client for top-level server
-time.sleep(5)
+# time.sleep(5)
 try:
     cts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("[CTS]: Client socket created")
+    print("[CTS]: Client socket for TS created")
 except socket.error as err:
     print('socket open error: {} \n'.format(err))
     exit()
 
-port = 50007
-localhost_addr = socket.gethostbyname(socket.gethostname())
-server_binding = (localhost_addr, port)
+ts_port = int(sys.argv[3])
+
+if "NS" in decoded_msg:
+    host = name[0]
+else:
+    host = socket.gethostname()
+
+#print(host)
+
+localhost_addr = socket.gethostbyname(host)
+server_binding = (localhost_addr, ts_port)
 cts.connect(server_binding)
 
-# "main method"
+cts.send(msg.encode('utf-8'))
 
-#if __name__ == "__main__":
-#    t1 = threading.Thread(name='root-server', target=server)
-#    t1.start()
+# Receive data from the server
+data_from_ts = cts.recv(100)
+decoded = data_from_ts.decode('utf-8')
+print("[C]: Data received from top-level-server: " + str(decoded))
 
-#    time.sleep(random.random() * 5)
-#    t2 = threading.Thread(name='top-level-server', target=server)
-#    t2.start()
-
-#    t3 = threading.Thread(name='client', target=client)
-#    t3.start()
-
-#    time.sleep(5)
-#    print('done')
+# write received data to outupt file
+with open('resolve.txt', 'a') as text_file:
+    text_file.write(decoded + "\n")

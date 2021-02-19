367187dda073a1c1c0c87245d524729eccfd2595
aum05 <78663514+aum05@users.noreply.github.com>
2021-02-20 01:12:54 +0530

    Update rs.py

--- rs.py
+++ rs.py
@@ -14,13 +14,12 @@ except socket.error as err:
 
 server_binding = ('', int(sys.argv[1]))
 rs.bind(server_binding)
-rs.listen(1)
+rs.listen(20)
 host = socket.gethostname()
 print("[RS]: Server host name is {}".format(host))
 localhost_ip = (socket.gethostbyname(host))
 print("[RS]: Server IP address is {}".format(localhost_ip))
-csockid, addr = rs.accept()
-print ("[RS]: Got a connection request from a client at {}".format(addr))
+
 
 hosts = []
 ip = []
@@ -35,22 +34,25 @@ with open('PROJI-DNSRS.txt', 'r') as file:
 dns = {"hostName" : hosts, "IP": ip, "flag": fl}
 
 while True:
-    # Receive data from the server
-    data_from_client = csockid.recv(100)
-    message = data_from_client.decode('utf-8')
-    print("[S]: Query received from client : {}".format(message))
+    csockid, addr = rs.accept()
+    print ("[RS]: Got a connection request from a client at {}".format(addr))
+    try:
+        while True:
+            # Receive data from the server
+            data_from_client = csockid.recv(1024)
+            message = data_from_client.decode('utf-8')
+            print("[S]: Query received from client : {}".format(message))
 
-    # Lookup the queried domain in the RS-DNS table
-    host_name = [name.lower() for name in dns["hostName"]]
+            # Lookup the queried domain in the RS-DNS table
+            host_name = [name.lower() for name in dns["hostName"]]
 
-    if message.lower() in host_name:
-        ind = host_name.index(message.lower())
-    else:
-        ind = dns["flag"].index("NS")
+            if message.lower() in host_name:
+                ind = host_name.index(message.lower())
+            else:
+                ind = dns["flag"].index("NS")
 
-    # Send either the resolved DNS or the TShostname
-    msg = dns["hostName"][ind] + " " + dns["IP"][ind] + " " + dns["flag"][ind]
-    csockid.send(msg.encode('utf-8'))
-
-rs.close()
-exit()
+            # Send either the resolved DNS or the TShostname
+            msg = dns["hostName"][ind] + " " + dns["IP"][ind] + " " + dns["flag"][ind]
+            csockid.sendall(msg.encode('utf-8'))
+    except:
+        pass
